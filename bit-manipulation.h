c6172e22128d653b2f88d4e6033282fc17bed83c
Devharsh Trivedi <dtrivedi@bowiestate.edu>
2024-10-23 15:22:44 -0400

    Update bit-manipulation.h

--- bit-manipulation.h
+++ bit-manipulation.h
@@ -1,1 +1,12 @@
+#include <stdio.h>
 
+
+// Function to display menu
+void displayMenu(void) {
+    printf("\nBit Manipulation Menu\n");
+    printf("-------------------\n");
+    printf("1: Set a bit\n");
+    printf("2: Clear a bit\n");
+    printf("3: Print the integer\n");
+    printf("4: Exit the program\n");
+}
